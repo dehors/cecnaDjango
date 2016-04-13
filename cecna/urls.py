41f39711a19c2c1bd78a095b88bf252291a79b2a
luismartinez24 <luismartinez242011@gmail.com>
2016-04-12 21:54:32 -0600

    feat: add templates

--- cecna/urls.py
+++ cecna/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'cecna.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('cecnaapp.urls')),
+    url(r'^', include('cecnaapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
